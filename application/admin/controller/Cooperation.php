d5a429bc2efce1ef49c28022330cc21bbcd4be84
xiaobu <269899062@qq.com>
2019-03-30 09:41:21 +0800

    3.30

--- application/admin/controller/Cooperation.php
+++ application/admin/controller/Cooperation.php
@@ -24,7 +24,7 @@ class Cooperation extends Base{
     	return view();
     }
 
-	//删除文章
+	//删除
 	public function del(){
 		$request = Request::instance();
 		if($request->isPost()){
@@ -39,4 +39,40 @@ class Cooperation extends Base{
 			return json($msg);
 		}
 	}
+
+    //导出
+    public function export()
+    {
+        $list = Db::name('cooperation')->select();
+        $type = getCooperationType();
+
+        $strTable ='<table width="500" border="1">';
+        $strTable .= '<tr>';
+        $strTable .= '<td style="text-align:center;font-size:12px;" width="*">ID</td>';
+        $strTable .= '<td style="text-align:center;font-size:12px;" width="*">姓名</td>';
+        $strTable .= '<td style="text-align:center;font-size:12px;" width="*">联系人电话</td>';
+        $strTable .= '<td style="text-align:center;font-size:12px;" width="*">公司</td>';
+        $strTable .= '<td style="text-align:center;font-size:12px;" width="*">职位</td>';
+        $strTable .= '<td style="text-align:center;font-size:12px;" width="*">合作方式</td>';
+        $strTable .= '<td style="text-align:center;font-size:12px;" width="*">报名日期</td>';
+        $strTable .= '</tr>';
+        if(is_array($list)){
+            foreach($list as $k=>$val){
+                $strTable .= '<tr>';
+                $strTable .= '<td style="text-align:center;font-size:12px;">&nbsp;'.$val['id'].'</td>';
+                $strTable .= '<td style="text-align:left;font-size:12px;">'.$val['name'].' </td>';
+                $strTable .= '<td style="text-align:left;font-size:12px;">'.$val['tel'].'</td>';
+                $strTable .= '<td style="text-align:left;font-size:12px;">'.$val['company'].' </td>';
+                $strTable .= '<td style="text-align:left;font-size:12px;">'.$val['position'].' </td>';
+                $strTable .= '<td style="text-align:left;font-size:12px;">'.$type[$val['cooperation_ways']].' </td>';
+                $strTable .= '<td style="text-align:left;font-size:12px;">'.$val['created_on'].' </td>';
+                $strTable .= '</tr>';
+            }
+        }
+        $strTable .='</table>';
+        unset($list);
+        downloadExcel($strTable,'商务合作报名信息');
+        exit();
+    }
+
 }
