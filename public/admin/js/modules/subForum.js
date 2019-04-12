b3c75b9aa2717ffef09dffeee3970c3b24de3358
xiaobu <269899062@qq.com>
2019-04-12 11:41:14 +0800

    4.11

--- public/admin/js/modules/subForum.js
+++ public/admin/js/modules/subForum.js
@@ -36,5 +36,5 @@ layui.define(['element', 'layer', 'form','base','laydate','layedit'], function(e
 		return false;
 	});
 
-	exports('content', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
+	exports('subforum', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
 });
