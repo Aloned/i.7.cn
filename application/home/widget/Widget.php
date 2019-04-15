68e68de12cf3867cee739ce63603ccbae65f544f
xiaobu <269899062@qq.com>
2019-04-15 14:06:01 +0800

    4.15

--- application/home/widget/Widget.php
+++ application/home/widget/Widget.php
@@ -14,6 +14,10 @@ class Widget extends controller
 		$topcate = Db::name('category')->where('is_show','1')->order('sort_order asc')->column('cat_id,parent_id,cat_name,sort_order,keywords');
 		
 		$catelist = get_parent_list($topcate,0);
+
+        $subforums = Db::name('parallel_session')->where('status','1')->order('id desc')->field('title,id')->select();
+
+        $this->assign('subforums',$subforums);
 		
 		$this->assign('list',$catelist);
 
