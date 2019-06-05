a775c4058a0a12ddb5c79f0650013a5007976ac9
xiaobu <269899062@qq.com>
2019-06-05 14:51:10 +0800

    6.15

--- application/mobile/controller/Online.php
+++ application/mobile/controller/Online.php
@@ -12,6 +12,9 @@ class Online extends Base
 	//联系我们
     public function index()
     {
+        if(session('userid')){
+            $this->redirect('/mobile/my');
+        }
     	$this->assign('nav',0);
 		return view();
     }
