c0b817d4b3704de68654a8efd9b5b513b14e148a
xiaobu <269899062@qq.com>
2019-06-22 16:55:59 +0800

    微信

--- application/mobile/controller/Base.php
+++ application/mobile/controller/Base.php
@@ -12,6 +12,10 @@ class Base extends Controller {
      */
     public function _initialize() 
     {
+        //微信端屏蔽
+        if($this->is_weixin()){
+            echo '网站筹备中...';die;
+        }
     	$request = Request::instance();
     	//select 得到二维数组  find 得到一维数组
 		$website = Db::name('config') -> where('id',1) -> find();
@@ -19,5 +23,11 @@ class Base extends Controller {
 		$joinnum = Db::name('user') -> count();
 		$this ->assign('joinnum',$joinnum);
 		$this -> assign('website',$website);
+    }
+
+    private function is_weixin() {
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) {
+            return true;
+        } return false;
     }
 }
